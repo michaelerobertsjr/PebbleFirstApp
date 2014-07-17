b2ec439b726b9746e5731b2d275e8fc56c46567d
Michael Roberts Jr <michael.e.roberts.jr@gmail.com>
2014-07-16 20:43:58 -0700

    Added GeoLocation

--- src/app.js
+++ src/app.js
@@ -21,3 +21,11 @@ simply.setText({
   body: 'This is a demo. Press buttons or tap the watch!',
 }, true);
 
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
+});
